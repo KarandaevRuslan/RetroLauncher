2b0eee795f149deb083a10f252798be78f5ef52d
Karandaev Ruslan <karandaevruslan@gmail.com>
2024-11-28 17:04:38 +0700

    Updated sh script

--- scripts/RetroLauncher.sh
+++ scripts/RetroLauncher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 JLINK_VM_OPTIONS=
-bin/java $JLINK_VM_OPTIONS -m com.karandaev.retrolauncher/com.karandaev.retrolauncher.Main "$@"
+bin/java $JLINK_VM_OPTIONS -m com.karandaev.retrolauncher/com.karandaev.retrolauncher.Main "$@" >/dev/null 2>&1 &
