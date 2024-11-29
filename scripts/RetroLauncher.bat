d876564e303975c5eb0a4961e7f69867a40bd6fd
Karandaev Ruslan <karandaevruslan@gmail.com>
2024-11-29 11:29:43 +0700

    Fixed updates

--- scripts/RetroLauncher.bat
+++ scripts/RetroLauncher.bat
@@ -1,5 +1,5 @@
 @echo off
 set "SCRIPT_DIR=%~dp0"
 set "JLINK_VM_OPTIONS=-Dhttps.protocols=TLSv1.2,TLSv1.1,TLSv1"
-start "" "%SCRIPT_DIR%bin\javaw.exe" %JLINK_VM_OPTIONS% -m com.karandaev.retrolauncher/com.karandaev.retrolauncher.Main %*
+start "" /D "%SCRIPT_DIR%" "%SCRIPT_DIR%bin\javaw.exe" %JLINK_VM_OPTIONS% -m com.karandaev.retrolauncher/com.karandaev.retrolauncher.Main %*
 exit /b 0
--- scripts/RetroLauncher.sh
+++ scripts/RetroLauncher.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR"
 JLINK_VM_OPTIONS="-Dhttps.protocols=TLSv1.2,TLSv1.1,TLSv1"
 "$SCRIPT_DIR/bin/java" $JLINK_VM_OPTIONS -m com.karandaev.retrolauncher/com.karandaev.retrolauncher.Main "$@" >/dev/null 2>&1 &
